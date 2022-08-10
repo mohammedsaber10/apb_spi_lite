c7bb02c658f10a97d7ad20197b1172498b0baa5f
Mohamed Saber <mohammedsaber736@gmail.com>
2022-08-10 00:10:56 -0700

    modifying something in shifter

--- rtl/LR_shifter.v
+++ rtl/LR_shifter.v
@@ -5,25 +5,33 @@ parameter DATAWIDTH = 8
 (
 input                 clk_i,
 input                 rst_n,
-input [DATAWIDTH-1:0] data;
+input [DATAWIDTH-1:0] data,
 input                 ld,
 input                 sh_en,
 input                 sh_rl,  // if sh_rl = 1 --> shift right, sh_rl = 0 --> sh
 output                sdo
 );
 reg   [DATAWIDTH-1:0] shift_reg;
+reg   [DATAWIDTH-1:0] shift_reg_next;
+
+
+always@(*) begin
+if(ld)                    shift_reg_next = data;
+else if (sh_en &&  sh_rl) shift_reg_next = {1'b0, shift_reg[DATAWIDTH-1:1]};
+else if (sh_en && !sh_rl) shift_reg_next = {shift_reg[DATAWIDTH-2:0], 1'b0};
+else                      shift_reg_next = shift_reg;
+end
+
+
 
 always@(posedge clk_i, negedge rst_n) begin
-if(!rst_n) shift_reg <= 0;
-else if(ld) shfit_reg <= data;
-else if (sh_en && sh_rl)  shift_reg <= {1'b0, shift_reg[DATAWIDTH-1:1]};
-else if (sh_en && !sh_rl) shift_reg <= {shift_reg[DATAWIDTH-2:0], 1'b0};
+if (!rst_n)          shift_reg <= 0;
+else                 shift_reg <= shift_reg_next;
 end
-);
 
 // assign the serial data output bit to the LSB or MSB of the shifter only if the shifter is enabled,
 // else it will be assigned a zero if the shifter is disabled.
 
-assign sdo = (sh_en) && ((sh_rl)? shift_reg[0]: shift_reg[DATAWIDTH-1]);
+assign sdo = (sh_rl)? shift_reg[0]: shift_reg[DATAWIDTH-1];
 
 endmodule
